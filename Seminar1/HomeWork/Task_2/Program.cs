cba2b9a3f3eabeb65cdfc991ec92e256a858a429
Pavel Grosberg <grosberg15@gmail.com>
2022-10-14 11:43:16 +0300

    seminar HomeWork commit

---
+++ Seminar1/HomeWork/Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Enter a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+}
---
+++ Seminar1/HomeWork/Task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.
+*/
+
+Console.WriteLine("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int counter = 2;
+while (counter <= N)
+{
+    System.Console.WriteLine(counter);
+    counter += 2;
+}
