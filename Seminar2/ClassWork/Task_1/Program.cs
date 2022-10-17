945a0e0d51f0dc96538623700a04166a75a8d344
Pavel Grosberg <grosberg15@gmail.com>
2022-10-17 22:30:44 +0300

    seminar2 ClassWork Commit

---
+++ Seminar2/ClassWork/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая будет принимать на вход два
+числа и выводить, является ли первое число
+кратным второму. Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно*/
+
+int min = 10;
+int max = 99;
+int randomNumber1 = new Random().Next(min, max + 1);
+int randomNumber2 = new Random().Next(min, max + 1);
+
+// Console.WriteLine(randomNumber1);
+// Console.WriteLine(randomNumber2);
+
+
+if (randomNumber1 % randomNumber2 == 0)
+{
+    Console.WriteLine($"{randomNumber1}, {randomNumber2} -> {randomNumber1 / randomNumber2}");
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"{randomNumber1}, {randomNumber2} -> {randomNumber1 % randomNumber2}");
+}
+
---
+++ Seminar2/ClassWork/Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b || b * b == a)
+{
+    Console.WriteLine($"да");
+}
+else 
+{
+    Console.WriteLine($"нет");
+}
