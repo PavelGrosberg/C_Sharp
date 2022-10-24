6577ca922f7c1849f770db3689e25153b046d7e0
Pavel Grosberg <grosberg15@gmail.com>
2022-10-24 16:04:26 +0300

    Seminar3 Task0,1 commit

---
+++ Seminar3/ClassWork/Task_0/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.WriteLine("введите X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == 0 || y == 0)
+{
+    System.Console.WriteLine("X и Y не должны быть равны нулю");
+    return;
+}
+
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("1 четверть");
+}
+else if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("2 четверть");
+}
+else if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("3 четверть");
+}
+else if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("4 четверть");
+}
---
+++ Seminar3/ClassWork/Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("введите номер четверти: ");
+int squareNumber = Convert.ToInt32(Console.ReadLine());
+
+switch (squareNumber)
+{
+    case 1:
+        System.Console.WriteLine("X > 0, Y > 0");
+        break;
+    case 2:
+        System.Console.WriteLine("X < 0, Y > 0");
+        break;
+    case 3:
+        System.Console.WriteLine("X < 0, Y < 0");
+        break;
+    case 4:
+        System.Console.WriteLine("X > 0, Y < 0");
+        break;
+    default:
+        System.Console.WriteLine("Четвертей всего 4");
+        break;
+}
