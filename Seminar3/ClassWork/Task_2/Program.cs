88d4c787239e6b603a878ac4c344e2502c8bb41d
Pavel Grosberg <grosberg15@gmail.com>
2022-10-26 08:39:43 +0300

    Seminar3 ClassWork FinalCommit

--- Seminar3/ClassWork/Task_2/Program.cs
+++ Seminar3/ClassWork/Task_2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int x1 = ReadInt("Введите Х точки A: ");
+int y1 = ReadInt("Введите Y точки A: ");
+int x2 = ReadInt("Введите Х точки B: ");
+int y2 = ReadInt("Введите Y точки B: ");
+
+int sqrLenghtX = (x2 - x1) * (x2 - x1);
+int sqrLenghtY = (y2 - y1) * (y2 - y1);
+double distance = Math.Sqrt(sqrLenghtX + sqrLenghtY);
+System.Console.WriteLine("Расстояние равно " + distance);
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Seminar3/ClassWork/Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i <= number)
+{
+    System.Console.Write(i * i);
+    if (i < number) Console.Write(", ");
+    i++;
+}
