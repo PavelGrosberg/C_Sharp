04b90afb94ff5ddae6e69e32041ec701cb5387d8
Pavel Grosberg <grosberg15@gmail.com>
2022-10-26 22:41:56 +0300

    Seminar3 HomeWork Task_19 Commit

---
+++ Seminar3/HomeWork/Task_19/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+(Использовать только математические операции, нельзя использовать число как строку по типу number[1])
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите положительное пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstThreeDigits = number / 100;
+int lastThreeDigits = number % 1000;
+
+int i = 1;
+int digitMultiplier = 100;
+int lastThreeDigitsReversed = 0; 
+int tempNumber = number;
+
+while (i <= 3)
+{
+    int temp = tempNumber % 10;
+    tempNumber /= 10;
+    lastThreeDigitsReversed += temp * digitMultiplier;
+    digitMultiplier /= 10;
+    i++;
+}
+
+if (firstThreeDigits == lastThreeDigitsReversed) Console.WriteLine($"{number} -> да");
+else Console.WriteLine($"{number} -> нет");
