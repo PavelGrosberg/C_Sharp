a110db0a5a18f653a2b1e39ca6799d52883bfbd8
Pavel Grosberg <grosberg15@gmail.com>
2022-11-10 13:38:04 +0300

    Seminar4 HomeWork Task27 Recursion commit

---
+++ Seminar4/HomeWork/Task_27_Recursion/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = SumDig(n);
+System.Console.WriteLine($"{n} -> {sum}");
+
+
+int SumDig (int tempN) 
+{ 
+    if (tempN == 0) return 0;
+    return (tempN % 10 + SumDig(tempN/10));
+}
