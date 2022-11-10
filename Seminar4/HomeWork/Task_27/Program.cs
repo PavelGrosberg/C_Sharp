4f8cc89d13b7cd81747c0f1d092619aae289d212
Pavel Grosberg <grosberg15@gmail.com>
2022-11-10 08:29:01 +0300

    Seminar4 HomeWork commit

---
+++ Seminar4/HomeWork/Task_27/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int tempN = n;
+while (tempN > 0)
+{
+    int digit = tempN % 10;
+    tempN /= 10;
+    sum +=digit ;
+}
+
+System.Console.WriteLine($"{n} -> {sum}");
---
+++ Seminar4/HomeWork/Task_29/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] setArray(int N, int min, int max)
+{
+   int[] nums = new int[N];
+   for (int i = 0; i < N; i++)
+   {
+        nums[i] = new Random().Next(min, max);
+        if (i == N - 1) System.Console.Write($"{nums[i]} -> "); 
+        else System.Console.Write($"{nums[i]}, ");
+   }
+   for (int i = 0; i < N; i++)
+   {
+        if (i == 0) System.Console.Write($"[{nums[i]}, ");
+        else if (i == N - 1) System.Console.Write($"{nums[i]}]");
+        else System.Console.Write($"{nums[i]}, "); 
+   }
+   
+   return nums;
+}
+
+
+setArray(10, 1, 10);
